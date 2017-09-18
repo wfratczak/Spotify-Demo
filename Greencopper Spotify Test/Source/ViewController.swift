d699a59230eeeb1d3cac1326c75eb64c56829f07
Wojciech Frątczak <fratczak.wojciech@gmail.com>
2017-09-18 00:45:34 -0400

    #2 Spotify SDK included

---
+++ Greencopper Spotify Test/Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Greencopper Spotify Test
+//
+//  Created by Wojtek Frątczak on 2017-09-17.
+//  Copyright © 2017 Wojtek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
